f071ac6380d30a7686d839e35caeb1c86fa41f8b
Younghyun SEO <tigerseo7686@gmail.com>
2024-09-26 02:21:27 +0900

    _javascript/pwa/sw.js_change

--- _javascript/pwa/sw.js
+++ _javascript/pwa/sw.js
@@ -1,94 +1,108 @@
-import { baseurl } from '../../_config.yml';
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
 
-importScripts(`${baseurl}/assets/js/data/swconf.js`);
-
-const purge = swconf.purge;
-const interceptor = swconf.interceptor;
-
-function verifyUrl(url) {
-  const requestUrl = new URL(url);
-  const requestPath = requestUrl.pathname;
-
-  if (!requestUrl.protocol.startsWith('http')) {
-    return false;
-  }
-
-  for (const prefix of interceptor.urlPrefixes) {
-    if (requestUrl.href.startsWith(prefix)) {
-      return false;
-    }
-  }
-
-  for (const path of interceptor.paths) {
-    if (requestPath.startsWith(path)) {
-      return false;
-    }
-  }
-  return true;
-}
-
-self.addEventListener('install', (event) => {
-  if (purge) {
-    return;
-  }
-
-  event.waitUntil(
-    caches.open(swconf.cacheName).then((cache) => {
-      return cache.addAll(swconf.resources);
+self.addEventListener('activate', function(e) {
+  self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll();
     })
-  );
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url))
+    });
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (purge) {
-            return caches.delete(key);
-          } else {
-            if (key !== swconf.cacheName) {
-              return caches.delete(key);
-            }
-          }
-        })
-      );
-    })
-  );
-});
+// import { baseurl } from '../../_config.yml';
 
-self.addEventListener('message', (event) => {
-  if (event.data === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+// importScripts(`${baseurl}/assets/js/data/swconf.js`);
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.headers.has('range')) {
-    return;
-  }
+// const purge = swconf.purge;
+// const interceptor = swconf.interceptor;
 
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
+// function verifyUrl(url) {
+//   const requestUrl = new URL(url);
+//   const requestPath = requestUrl.pathname;
 
-      return fetch(event.request).then((response) => {
-        const url = event.request.url;
+//   if (!requestUrl.protocol.startsWith('http')) {
+//     return false;
+//   }
 
-        if (purge || event.request.method !== 'GET' || !verifyUrl(url)) {
-          return response;
-        }
+//   for (const prefix of interceptor.urlPrefixes) {
+//     if (requestUrl.href.startsWith(prefix)) {
+//       return false;
+//     }
+//   }
 
-        // See: <https://developers.google.com/web/fundamentals/primers/service-workers#cache_and_return_requests>
-        let responseToCache = response.clone();
+//   for (const path of interceptor.paths) {
+//     if (requestPath.startsWith(path)) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
-        caches.open(swconf.cacheName).then((cache) => {
-          cache.put(event.request, responseToCache);
-        });
-        return response;
-      });
-    })
-  );
-});
+// self.addEventListener('install', (event) => {
+//   if (purge) {
+//     return;
+//   }
+
+//   event.waitUntil(
+//     caches.open(swconf.cacheName).then((cache) => {
+//       return cache.addAll(swconf.resources);
+//     })
+//   );
+// });
+
+// self.addEventListener('activate', (event) => {
+//   event.waitUntil(
+//     caches.keys().then((keyList) => {
+//       return Promise.all(
+//         keyList.map((key) => {
+//           if (purge) {
+//             return caches.delete(key);
+//           } else {
+//             if (key !== swconf.cacheName) {
+//               return caches.delete(key);
+//             }
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
+
+// self.addEventListener('message', (event) => {
+//   if (event.data === 'SKIP_WAITING') {
+//     self.skipWaiting();
+//   }
+// });
+
+// self.addEventListener('fetch', (event) => {
+//   if (event.request.headers.has('range')) {
+//     return;
+//   }
+
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       if (response) {
+//         return response;
+//       }
+
+//       return fetch(event.request).then((response) => {
+//         const url = event.request.url;
+
+//         if (purge || event.request.method !== 'GET' || !verifyUrl(url)) {
+//           return response;
+//         }
+
+//         // See: <https://developers.google.com/web/fundamentals/primers/service-workers#cache_and_return_requests>
+//         let responseToCache = response.clone();
+
+//         caches.open(swconf.cacheName).then((cache) => {
+//           cache.put(event.request, responseToCache);
+//         });
+//         return response;
+//       });
+//     })
+//   );
+// });
